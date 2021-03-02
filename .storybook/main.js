35889fb482ff5a11ce516ec660cfe63b4fbcf658
李旭峰 <lxf@asman.com.cn>
2021-03-02 11:55:41 +0800

    init pro

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../components/**/*.stories.mdx",
+    "../components/button/Button.stories.@(js|jsx|ts|tsx)",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
