d49b23409d71dad20a39ab11e38ec987cd6042c8
李旭峰 <lxf@asman.com.cn>
2021-03-02 19:51:28 +0800

    .

--- components/taskList/TaskList.js
+++ components/taskList/TaskList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../../src/index.css";
-
+import PropTypes from "prop-types";
 import {Task} from "../task/Task";
 
 export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask}) => {
@@ -58,4 +58,18 @@ export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask}) => {
             }
         </div>
     );
+};
+
+TaskList.propTypes = {
+    /** Checks if it's in loading state */
+    loading: PropTypes.bool,
+    /** The list of tasks */
+    tasks: PropTypes.arrayOf(Task.propTypes).isRequired,
+    /** Event to change the task to pinned */
+    onPinTask: PropTypes.func,
+    /** Event to change the task to archived */
+    onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+    loading: false,
 }
