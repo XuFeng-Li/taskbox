bc640d153863f9be90287e1b0108399d368c6563
李旭峰 <lxf@asman.com.cn>
2021-03-03 10:03:06 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, {Component} from "react";
+import { Provider } from "react-redux";
+import store from "./store/redux";
+
+import InboxScreen from '../components/inboxScreen/InboxScreen';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  return (
+      <Provider>
+        <InboxScreen />
+      </Provider>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
