337e3983783c0be65531182eb2d50a19ea3d8c86
李旭峰 <lxf@asman.com.cn>
2021-03-05 02:18:11 +0800

    .

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,19 @@
 import babel from 'rollup-plugin-babel'; // 需要使用最新JS语法，babel 转码
-import commonjs from 'rollup-plugin-commonjs'; // rollup-plugin-node-resolve 插件可以解决 ES6模块的查找导入，但是npm中的大多数包都是以CommonJS模块的形式出现的，所以需要使用这个插件将CommonJS模块转换为 ES2015 供 Rollup 处理
+// import commonjs from 'rollup-plugin-commonjs'; // rollup-plugin-node-resolve 插件可以解决 ES6模块的查找导入，但是npm中的大多数包都是以CommonJS模块的形式出现的，所以需要使用这个插件将CommonJS模块转换为 ES2015 供 Rollup 处理
 // import external from 'rollup-plugin-peer-deps-external'; // 打包的时候排除external列表中的包，作为外部依赖
 import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve'; // 帮助 Rollup 查找外部模块，然后安装
+// import resolve from 'rollup-plugin-node-resolve'; // 帮助 Rollup 查找外部模块，然后安装
 import json from "rollup-plugin-json";
 import url from 'rollup-plugin-url';
 import svgr from '@svgr/rollup';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import { terser } from "rollup-plugin-terser";
+// import { terser } from "rollup-plugin-terser";
 import path from 'path';
-const cwd = process.cwd()
+const cwd = process.cwd();
 const pkgPath = path.resolve(cwd, './package.json');
-const pkg = require(pkgPath)
-const mainPkg = require('./package.json')
-
-console.log("****************************************  pkgPath");
-console.log(pkgPath);
-console.log("****************************************  mainPkg");
-console.log(mainPkg);
+const pkg = require(pkgPath);
+const mainPkg = require('./package.json');
 
 const externals = [
   ...Object.keys(pkg.peerDependencies || {}),
